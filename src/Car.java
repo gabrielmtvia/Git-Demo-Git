897263d732129c5ab586b0f88be488b816da1c02
Gabriel Moutinho <gabrielmt@via.dk>
2021-05-12 13:51:39 +0200

    car updated

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,33 @@
 public class Car
 {
+  private String make;
+  private String RegPlate;
+
+  public String getMake()
+  {
+    return make;
+  }
+
+  public void setMake(String make)
+  {
+    this.make = make;
+  }
+
+  public String getRegPlate()
+  {
+    return RegPlate;
+  }
+
+  public void setRegPlate(String regPlate)
+  {
+    RegPlate = regPlate;
+  }
+
+  public Car(String make, String regPlate)
+  {
+    this.make = make;
+    this.RegPlate = regPlate;
+  }
+
+
 }
